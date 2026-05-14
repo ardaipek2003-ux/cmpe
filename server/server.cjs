1906f6502e958119e2c00fdbed9b9a633275afb2
Arda İpek <ardaipek2003@gmail.com>
2026-05-14 17:36:49 +0300

    Fix Express 5 wildcard routing error

--- server/server.cjs
+++ server/server.cjs
@@ -356,7 +356,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // Catch-all to serve index.html for single-page app routing
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
